a62fb576b9c8033aab0d2c7e2cd56acf80907e47
CodeCanna <codecannamw@gmail.com>
2024-09-07 08:05:52 -0600

    Added some getters and setters.

--- php/objects/Notes.php
+++ php/objects/Notes.php
@@ -6,12 +6,33 @@ class Notes extends Problems
     protected bool $isFruiting = false;
     protected int $numberOfFruits = 0;
     protected string $fertilizerUsed = "";
-    protected float $fertilizerAmount = 0.0; // Weight
+    protected float $fertilizerWeight = 0.0; // Weight
     protected array $npk = array(
         "n" => 0.0,
         "p" => 0.0,
         "k" => 0.0
     );
+    protected Problems $problems;
+
+    public function __construct(
+        bool $isFlowering,
+        bool $isFruiting,
+        int $numberOfFruits,
+        string $fertilizerUsed,
+        float $fertilizerWeight,
+        array $npk,
+        Problems $problems
+    )
+    {
+        $this->isFlowering = $isFlowering;
+        $this->isFruiting = $isFruiting;
+        $this->numberOfFruits = $numberOfFruits;
+        $this->fertilizerUsed = $fertilizerUsed;
+        $this->fertilizerWeight = $fertilizerWeight;
+        $this->npk = $npk;
+        $this->problems = $problems;
+    }
+
     
     // $isFlowering //
     public function getIsFlowering(): bool
@@ -45,7 +66,38 @@ class Notes extends Problems
     {
         $this->numberOfFruits = $numberOfFruits;
     }
+
+    // Fertilizer Used //
+    public function getFertilizerUsed(): string
+    {
+        return $this->fertilizerUsed;
+    }
+
+    public function setFertilizerUsed(string $fertilizerUsed)
+    {
+        $this->fertilizerUsed = $fertilizerUsed;
+    }
     
-    
+    // Fertilizer Weight //
+    public function getFertilizerWeight(): float
+    {
+        return $this->fertilizerWeight;
+    }
+
+    public function setFertilizerWeight(float $fertilizerWeight)
+    {
+        $this->fertilizerWeight = $fertilizerWeight;
+    }
+
+    // NPK //
+    public function getNPK(): array
+    {
+        return $this->npk;
+    }
+
+    public function setNPK(array $npk)
+    {
+        $this->npk = $npk;
+    }
 }
 ?>
