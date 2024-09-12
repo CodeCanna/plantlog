9a3df5fa007a3aa0193fd1bff58e16559d8a3a51
CodeCanna <codecannamw@gmail.com>
2024-09-11 21:53:16 -0600

    Added some missing fields.

--- php/pages/newlog.php
+++ php/pages/newlog.php
@@ -5,6 +5,7 @@
         <input type="text" id="plant-name" name="plantname" required><br>
         <label for="log-date-time">Date:</label><br>
         <input type="datetime-local" id="log-date-time" name="log-date-time" /><br>
+        <h1>Fertilizer</h1>
         <label>Did you Fertilize?</label><br>
         <input type="radio" id="fertilize-yes" name="did-fertilize" value="true">
         <label for="fertilize-yes">Yes</label>
@@ -12,31 +13,43 @@
         <label for="fertilize-no">No</label><br>
         <div id="fertilizer-form"><!-- Hidden form revealed if yes radio button is clicked -->
             <label for="fertilizer-used">Fertilizer Used:</label><br>
-            <input type="text" id="fertilizer-used">
+            <input type="text" id="fertilizer-used" name="fertilizer-used">
             <label for="fertilizer-weight">Fertilizer Weight (Grams):</label>
-            <input type="number" step="0.1" id="fertilizer-weight" min="0">
-            <label for="npk">NPK</label>
+            <input type="number" step="0.1" id="fertilizer-weight" name="fertilizer-weight" min="0">
+            <h3>NPK</h3>
             <span>N:</span>
-            <input type="number" step="0.1" id="fertilizer-weight" min="0">
+            <input type="number" step="0.1" id="fertilizer-n" name="fertilizer-n" min="0">
             <span>P:</span>
-            <input type="number" step="0.1" id="fertilizer-weight" min="0">
+            <input type="number" step="0.1" id="fertilizer-p" name="fertilizer-p" min="0">
             <span>K:</span>
-            <input type="number" step="0.1" id="fertilizer-weight" min="0">
+            <input type="number" step="0.1" id="fertilizer-k" name="fertilizer-k" min="0">
         </div>
-        <label>Problems</label><!-- Problems Form -->
+        <h1>Problems</h1><!-- Problems Form -->
         <label for="problem-name">Name</label>
-        <input type="text" name="problem-name" id="problem-name">
+        <input type="text" id="problem-name" name="problem-name">
         <label>Is It Treatable?</label><br>
         <input type="radio" id="treatable-yes" name="treatable" value="true">
         <label for="yes">Yes</label>
         <input type="radio" id="treatable-no" name="treatable" value="false">
         <label for="no">No</label><br>
-        <label for="research:">Research</label>
-        <textarea name="research" id="research"></textarea>
         <label for="treatments-tried">Treatements Tried:</label>
         <textarea id="treatments-tried" name="treatments-tried"></textarea>
         <label for="treatments-found">Treatements Found:</label>
         <textarea id="treatments-found" name="treatments-found"></textarea>
+        <h1>Notes</h1>
+        <label for="research:">Research</label><!-- Research on the plant not a particular disease or problem but could include such things -->
+        <textarea id="research" name="research"></textarea>
+        <label>Is the plant flowering?</label><br>
+        <input type="radio" id="flowering-yes" name="is-flowering" value="true">
+        <label for="fertilize-yes">Yes</label>
+        <input type="radio" id="flowering-no" name="is-flowering" value="false" checked="checked"><br>
+        <label>Is the plant fruiting?</label><br>
+        <input type="radio" id="fruiting-yes" name="is-fruiting" value="true">
+        <label for="fruiting-yes">Yes</label>
+        <input type="radio" id="fruiting-no" name="is-fruiting" value="false" checked="checked">
+        <label for="fruiting-no">No</label><br>
+        <label for="number-of-fruits">How many fruits?</label>
+        <input type="number" id="number-of-fruits" name="number-of-fruits">
         <label for="images">Upload Images:</label>
         <input type="file" id="images" name="images" multiple="multiple">
         <button type="submit">Create Log</button>
